77a0ea5aaae7922e8460927e7d78d81c65acb1cd
Lirac <Liraclomex@yahoo.com>
2022-07-14 13:47:21 +0100

    Authorization for home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,7 @@ export async function getServerSideProps(context) {
         return {
             redirect:{
                 destination: '/login',
-                permanent: false
+                permanent: false,
             }
         }
     }
